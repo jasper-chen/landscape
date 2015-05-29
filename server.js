bf0c09da375e9977dc28ccd752060da96dde9d88
jaazpa3 <jaazpa3@gmail.com>
2015-05-29 13:44:55 -0700

    creating hashtables

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ var express = require('express');
 var request = require('request');
 var path = require('path');
 var app = express();
-var http = require('http');
 var _s = require('underscore.string');
 var debug = require('debug')('horses');
 
@@ -14,58 +13,62 @@ app.use(express.static('public'));
 //routes 
 
 app.get('/api/gps', function(req, res){
-	request('http://api.landscape-computing.com/nboxws/rest/v1/zone/pa_1/?key=76e253a5c51ecf1dbf17e9ea6b9d6a2f', function (err, response, body) {
-	  	if (!err && response.statusCode == 200) {
-	  		
-			//XML
-			if (_s(body).startsWith("<")) {
-				//XML -> JSON
 
-				parsedJSON = parseString(body, function(err, result) {
+	request('http://api.landscape-computing.com/nboxws/rest/v1/site/pa/query/summary/?key=76e253a5c51ecf1dbf17e9ea6b9d6a2f', function (err, response, body) {
+		var occupancyArray = new Array();
+		var res = body.split("|");
+		res.shift();
+		res.pop();
+		for (var i = 0; i < res.length; i++){
+			sensorId = res[i].split(":")[0];
+			occupied = res[i].split(":")[1][1];
+			occupancyArray[sensorId] = occupied;
+		}
+	});
+
+	//then do this...
+
+	request('http://api.landscape-computing.com/nboxws/rest/v1/zone/pa_1/?key=76e253a5c51ecf1dbf17e9ea6b9d6a2f', function (err, response, body) {
+
+	  	if (!err && response.statusCode == 200) {
+			
+			if (_s(body).startsWith("<")) {
+				//if XML, then convert to JSON 
+
+				parseString(body, function(err, result) {
 
 		    		if (err) {
 		    			debug(err)
-		  			}
+		  			} else {
 
-		  			else {
+		  				sensors = result['zone']['sensorIdList'][0]['sensorId'];
 
+		  				var array = new Array();
 
-		  				debug(''+'XML'+JSON.stringify(result));
-		  				
-		  				parsed = result["zone"];
-				    	//Hashtable for sensor_id and coordinates
-				    	//var array = new Array();
-
-				    	//for (var i = 0; i < Object.keys(sensorList).length; i++) {
-
-				    		//array[sensorList[i]['guid'].toString()] = sensorList[i]['gpsCoordList'].toString();	
-			    		return JSON.stringify(parsed)
+		  				for (var i = 0; i < Object.keys(sensors).length; i++){
+		  					coordinates = sensors[i]["gpsCoordList"][0]["gpsCoord"];
+		  					sensorId = sensors[i]["guid"][0].toString();
+		  					array[sensorId] = coordinates;
+		  				}
 		    		}
-		    	
 				})
 
 			} else {
-				debug('JSON'+body);
-				parsedJSON = body;
+				result = JSON.parse(body)['sensorId'];
+				var coordinatesArray = new Array();
+
+				for (var i = 0; i < Object.keys(result).length; i++){
+					coordinates = result[i]["gpsCoord"];
+					sensorId = result[i]["guid"].toString();
+					coordinatesArray[sensorId] = coordinates;
+				}
+			debug(array);
 			}
-
-			debug('RESULT' + JSON.stringify(parsedJSON));
-
-			//var sensorList = parsedJSON['zone']['sensorIdList']['0']['sensorId'];
-
-			res.send(parsedJSON);
-
-		}
-		
-	    else {
-	  		res.send('broken_b');
+		} else {
+	  		debug('broken_b');
 	    }
-
-	  //upon success of creating the hashtable
 	})
 });
-
-
 
 app.get('*', function(req, res){
 	res.sendFile(path.join(__dirname + '/public/index.html'));
