24a712ff494409f1113dffeccc3b0b82b34b59c9
jaazpa3 <jaazpa3@gmail.com>
2015-05-29 14:42:28 -0700

    server.js

--- server.js
+++ server.js
@@ -10,24 +10,27 @@ var APIKEY = '76e253a5c51ecf1dbf17e9ea6b9d6a2f';
 
 app.use(express.static('public'));
 
+//functions
+
+function occupancyCall() {
+	request('http://api.landscape-computing.com/nboxws/rest/v1/site/pa/query/summary/?key=76e253a5c51ecf1dbf17e9ea6b9d6a2f', function (err, response, body) {
+
+			var occupancyArray = new Array();
+			var res = body.split("|");
+			res.shift();
+			res.pop();
+			debug('creating occupany hashtable...')
+			for (var i = 0; i < res.length; i++){
+				sensorId = res[i].split(":")[0];
+				occupied = res[i].split(":")[1][1];
+				occupancyArray[sensorId] = occupied;
+			}
+	})
+}
+
 //routes 
 
 app.get('/api/gps', function(req, res){
-
-	request('http://api.landscape-computing.com/nboxws/rest/v1/site/pa/query/summary/?key=76e253a5c51ecf1dbf17e9ea6b9d6a2f', function (err, response, body) {
-		var occupancyArray = new Array();
-		var res = body.split("|");
-		res.shift();
-		res.pop();
-		for (var i = 0; i < res.length; i++){
-			sensorId = res[i].split(":")[0];
-			occupied = res[i].split(":")[1][1];
-			occupancyArray[sensorId] = occupied;
-		}
-	});
-
-	//then do this...
-
 	request('http://api.landscape-computing.com/nboxws/rest/v1/zone/pa_1/?key=76e253a5c51ecf1dbf17e9ea6b9d6a2f', function (err, response, body) {
 
 	  	if (!err && response.statusCode == 200) {
@@ -38,10 +41,14 @@ app.get('/api/gps', function(req, res){
 				parseString(body, function(err, result) {
 
 		    		if (err) {
+		    			//XML parsing not successful
 		    			debug(err)
 		  			} else {
+		  				debug('parsing XML...');
 
 		  				sensors = result['zone']['sensorIdList'][0]['sensorId'];
+
+		  				debug('creating XML hashtable...');
 
 		  				var array = new Array();
 
@@ -51,10 +58,14 @@ app.get('/api/gps', function(req, res){
 		  					array[sensorId] = coordinates;
 		  				}
 		    		}
+		    		debug(array);
 				})
 
 			} else {
 				result = JSON.parse(body)['sensorId'];
+
+				debug('creating JSON hashtable...');
+
 				var coordinatesArray = new Array();
 
 				for (var i = 0; i < Object.keys(result).length; i++){
@@ -62,10 +73,12 @@ app.get('/api/gps', function(req, res){
 					sensorId = result[i]["guid"].toString();
 					coordinatesArray[sensorId] = coordinates;
 				}
-			debug(array);
+				debug(coordinatesArray);
 			}
+		debug('calling for sensor occupancies...');
+		occupancyCall();
 		} else {
-	  		debug('broken_b');
+	  		//contains error or request not successful
 	    }
 	})
 });
